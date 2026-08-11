36236c2982d263041656228868af1d4d3653d4df
Kola0D246 <koustubhsinhapolytecnic@gmail.com>
2026-08-11 18:11:43 +0530

    add link to update model

--- models/updateModel.js
+++ models/updateModel.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import z from "zod";
 import { addPublicId, hideMongoInternals } from "../utils/schemaPlugins.js";
+import { urlField } from "../utils/zodHelpers.js";
 
-// update dont need to be archieved
 const updateSchema = new mongoose.Schema({
-    message: { type: String, required: true }
+    message: { type: String, required: true },
+    link: { type: String }
 }, {
     timestamps: true
 });
@@ -15,7 +16,8 @@ hideMongoInternals(updateSchema);
 export default mongoose.model('Update', updateSchema, 'updates');
 
 export const createUpdateSchema = z.object({
-    message: z.string().min(1).max(1000)
+    message: z.string().min(1).max(1000),
+    link: urlField()
 }).strict();
 
 export const updateUpdateSchema = createUpdateSchema.partial().strict();
